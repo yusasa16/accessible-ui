5aa29dcee2a0aabfe68e4b16d5f50c3df3b5e761
yusasa16 <yusasa16@gmail.com>
2023-01-23 23:37:47 +0900

    モーダル

---
+++ htdocs/dialog/src/index.ts
@@ -0,0 +1,8 @@
+import 'bulma'
+import './style.scss'
+
+const openBtn = document.getElementById('openBtn');
+const dialog = document.getElementById('dialog');
+const closeBtn = document.getElementById('closeBtn');
+
+console.log(openBtn, dialog, closeBtn);
