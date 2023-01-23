33bd06e6a5ca39f8ac8b44df82c494f894de8c4b
yusasa16 <yusasa16@gmail.com>
2023-01-23 23:41:41 +0900

    モーダル

--- htdocs/dialog/src/index.ts
+++ htdocs/dialog/src/index.ts
@@ -6,3 +6,13 @@ const dialog = document.getElementById('dialog');
 const closeBtn = document.getElementById('closeBtn');
 
 console.log(openBtn, dialog, closeBtn);
+
+if(openBtn && dialog && closeBtn) {
+	openBtn.addEventListener('click', function() {
+		dialog.show();
+	});
+
+	closeBtn.addEventListener('click', function() {
+		dialog.close();
+	})
+}
