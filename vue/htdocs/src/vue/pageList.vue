a06a0aa1fbd77c5a43292935970b4a83351bcc15
yusasa16 <yusasa16@gmail.com>
2023-02-19 22:54:20 +0900

    ディレクトリ構造変更

---
+++ vue/htdocs/src/vue/pageList.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const partsList = [
+	{
+		heading: 'タブ',
+		description: 'タブ切り替えパーツ',
+		list: [
+			{
+				title: 'タブ-vue',
+				path: '/tab/vue/index.html',
+				description: 'vueでの作成'
+			},
+		]
+	},
+]
+</script>
+
+<template>
+	<div class="container">
+		<section class="section" v-for="item in partsList">
+			<h2 class="title is-spaced">{{ item.heading }}</h2>
+			<p class="subtitle" v-if="item.description">{{ item.description }}</p>
+			<ul v-for="list in item.list">
+				<li class="box mb-4">
+					<p class="title">
+						<a :href="list.path">{{ list.title }}</a>
+					</p>
+					<p class="subtitle" v-if="list.description">{{ list.description }}</p>
+					<!-- <p v-else>説明なし</p> -->
+				</li>
+			</ul>
+		</section>
+	</div>
+</template>
